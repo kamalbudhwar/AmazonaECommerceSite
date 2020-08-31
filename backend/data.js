3419deabcac7cb13b8b39e457a136d719df7b1a7
Kamal <er.kamalsandhu@gmailcom>
2020-08-31 14:08:16 +1000

    First Commit

---
+++ backend/data.js
@@ -0,0 +1,91 @@
+const data={
+    products:[{
+        _id:'1',
+        name:'Slim Shirt',
+        image:'/images/Slim-Fit-chain-collar-shirt-white.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'2',
+        name:'Slim Shirt',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'3',
+        name:'Slim Shirt',
+        image:'/images/d4.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'4',
+        name:'Slim Shirt',
+        image:'/images/d5.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'5',
+        name:'Slim Shirt',
+        image:'/images/d6.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'6',
+        name:'Slim Shirt',
+        image:'/images/d7.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'7',
+        name:'Slim Shirt',
+        image:'/images/d8.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'8',
+        name:'Slim Shirt',
+        image:'/images/d9.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },{
+        _id:'9',
+        name:'Slim Shirt',
+        image:'/images/d10.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    },{
+        _id:'10',
+        name:'Slim Shirt',
+        image:'/images/blue-shirt.jpg',
+        price:60,
+        brand:'styleWith',
+        rating:4.5,
+        numReviews:10
+    }]
+}   
+export default data;
